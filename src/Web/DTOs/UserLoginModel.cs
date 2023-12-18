873cb99c368c6558e5e29186f13a9cdfd25fc299
Rian Negreiros <riannegreiros@gmail.com>
2023-12-18 09:51:22 -0300

    feat(Web): add data annotations for validation

--- src/Web/DTOs/UserLoginModel.cs
+++ src/Web/DTOs/UserLoginModel.cs
@@ -1,7 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Web.DTOs;
 
 public class UserLoginModel
 {
-  public string Email { get; set; }
-  public string Password { get; set; }
+	[Required]
+	[EmailAddress]
+	public string Email { get; set; }
+	[Required]
+	public string Password { get; set; }
 }
--- src/Web/DTOs/UserRegistrationModel.cs
+++ src/Web/DTOs/UserRegistrationModel.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Web.DTOs;
 
 public class UserRegistrationModel
 {
-  public string Name { get; set; }
-  public string Email { get; set; }
-  public string Password { get; set; }
+	[Required]
+	public string Name { get; set; }
+	[Required]
+	[EmailAddress]
+	public string Email { get; set; }
+	[Required]
+	public string Password { get; set; }
 }
