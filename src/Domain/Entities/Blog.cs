35a37d5a2339d3306e73fe35fa99239af34f0848
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-12-21 11:58:59 -0300

    fix: typos

--- src/Domain/Entities/Blog.cs
+++ src/Domain/Entities/Blog.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Domain.Entities;
 
 public class Blog : BaseEntity
@@ -5,10 +7,10 @@ public class Blog : BaseEntity
 	public string Title { get; private set; }
 	public string Description { get; private set; }
 	public string? ImageUrl { get; private set; }
-	public string UserId { get; private set; }
-	public User user { get; private set; }
+	public int UserId { get; private set; }
+	public User User { get; private set; }
 	public DateTime CreatedAt { get; private set; }
 	public DateTime UpdatedAt { get; private set; }
-	public Category category { get; private set; }
-	public string CategoryId { get; private set; }
+	public int CategoryId { get; private set; }
+	public Category Category { get; private set; }
 }
