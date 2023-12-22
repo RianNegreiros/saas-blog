769e722264bd97177f4ed63b626aa13d0172e583
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-12-22 09:55:21 -0300

    feat: add `Appbar.tsx`

--- src/client/app/page.tsx
+++ src/client/app/page.tsx
@@ -1,13 +1,9 @@
 'use client'
 
-import { useSession } from 'next-auth/react'
-
 export default function Home() {
-  const { data, status } = useSession()
   return (
     <main>
       <div>Home</div>
-      <div>{JSON.stringify(data)}</div>
     </main>
   )
 }
