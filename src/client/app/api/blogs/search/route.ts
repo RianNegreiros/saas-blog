2cd40afc0eeddd6131cc2a303d4ac56b48619504
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-12-23 14:34:12 -0300

    style(client): run prettier

--- src/client/app/api/blogs/search/route.ts
+++ src/client/app/api/blogs/search/route.ts
@@ -1,15 +1,17 @@
-import { generateSuccessMessage, generateErrorMessage } from "@/lib/helpers"
+import { generateSuccessMessage, generateErrorMessage } from '@/lib/helpers'
 
 export const GET = async (req: Request) => {
-	const searchedTitle = new URL(req.url).searchParams.get("title")
+	const searchedTitle = new URL(req.url).searchParams.get('title')
 	try {
-
-		const response = await fetch(`${process.env.API_URL}/api/blogs/search?title=${searchedTitle}`, {
-			method: 'GET',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-		})
+		const response = await fetch(
+			`${process.env.API_URL}/api/blogs/search?title=${searchedTitle}`,
+			{
+				method: 'GET',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			}
+		)
 
 		if (response.ok) {
 			const blog = await response.json()
